c0f737ee5bf0a1eb4766ddf2f6bf99a2e8c2333d
Mateo <47215311+mlcsec@users.noreply.github.com>
2020-03-13 09:26:39 -0400

    Add files via upload

--- createWordlists.sh
+++ createWordlists.sh
@@ -5,7 +5,14 @@ if [ $# -ne 2 ]; then
     exit 1;
 fi
 
-sed -e "s/^/$1-/" $2 > $1-buckets.txt
-sed -e "s/$/-$1/" $2 >> buckets-$1.txt
+cp common.txt buckets-$1.txt
+cp common.txt buckets$1.txt
+
+sed -e "s/^/$1-/" $2 >> $1-buckets.txt
 sed -e "s/^/$1/" $2 >> $1buckets.txt
-sed -e "s/$/$1/" $2 >> buckets$1.txt
+
+#sed -e "s/$/-$1/" $2 >> buckets-$1.txt
+ex +"%s/$/-demo/g" -cwq buckets-$1.txt
+
+#sed -e "s/$/$1/" $2 >> buckets$1.txt
+ex +"%s/$/-demo/g" -cwq buckets$1.txt
